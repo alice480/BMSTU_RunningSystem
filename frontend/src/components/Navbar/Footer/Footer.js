fc8aa364e534e8d1818fd20248eb5de59941b9f9
Alice <sinyavina.alisa2003@mail.ru>
2023-12-07 22:46:28 +0300

    remove venv

---
+++ frontend/src/components/Navbar/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3 col-sm-6">
+                        <h4>Running System</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
